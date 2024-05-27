b8f62a49af1a7b4bc8f211a96498de4e2db3a7fa
NensiMakawana <nensi.makawana@gmail.com>
2024-05-27 17:37:38 +0530

    added code for string calculator and fixed test cases

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,5 +5,15 @@ class StringCalculator
 
   def add
     raise "Only string value allowed." if @numbers.class != String
+    number_array = @numbers.split(/[^0-9-]+/).map(&:to_i)
+    negatives_array = number_array.select{ |i| i<0 }
+    raise "Cannot accept negatives." if negatives_array.length > 0
+    total = number_array.inject(0){|sum,x| sum + x }
   end
 end
+
+# p StringCalculator.new("").add
+# p StringCalculator.new("1").add
+# p StringCalculator.new("1,2").add
+# p StringCalculator.new("1\n2").add
+# p StringCalculator.new("1\\;2,3").add
